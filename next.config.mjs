5c393052b77ac0444525686b90089a87b195882c
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-07-18 21:39:00 +0530

    update caching logic and add export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // for static export
+  experimental: {
+    // Enable caching layers
+    cacheDirs: [".next/cache", "node_modules/.cache/next-server"],
+  },
+};
 
 export default nextConfig;
