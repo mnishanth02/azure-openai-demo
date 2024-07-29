6faa2b0ada2004254eebcf3dd32f6802b460e149
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-07-29 19:36:26 +0530

    cahgne env

--- app/(main)/translate/page.tsx
+++ app/(main)/translate/page.tsx
@@ -1,17 +1,17 @@
-import { LANGUAGE_ENDPOINT } from "@/lib/constants";
+import { LANGUAGE_ENDPOINT, languages } from "@/lib/constants";
 
 import { TranslationLanguages } from "../_lib/types";
 import TranslateProvider from "./_lib/components/translate-provider";
 
-const TranslatePage = async () => {
-  const response = await fetch(LANGUAGE_ENDPOINT, {
-    next: {
-      revalidate: 60 * 60 * 24, //  cache for 24 hours
-    },
-  });
-  const languages = (await response.json()) as TranslationLanguages;
+const TranslatePage = () => {
+  // const response = await fetch(LANGUAGE_ENDPOINT, {
+  //   next: {
+  //     revalidate: 60 * 60 * 24, //  cache for 24 hours
+  //   },
+  // });
+  // const languages = (await response.json()) as TranslationLanguages;
 
-  return <TranslateProvider languages={languages} />;
+  return <TranslateProvider languages={languages as TranslationLanguages} />;
 };
 
 export default TranslatePage;
